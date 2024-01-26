0c631fdee976b64d16881cb4fc690fc4b70b1952
cassius <cassiusbessa@gmail.com>
2024-01-25 21:15:20 -0300

    docs: :memo: exposed route and implement swagger docs

--- main.go
+++ main.go
@@ -2,27 +2,53 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
+	bcrypt "github.com/Grupo-38-Orange-Juice/orange-portfolio-back/data/crypto"
+	http "github.com/Grupo-38-Orange-Juice/orange-portfolio-back/data/http"
 	"github.com/Grupo-38-Orange-Juice/orange-portfolio-back/data/postgres"
+	docs "github.com/Grupo-38-Orange-Juice/orange-portfolio-back/docs"
+	usecases "github.com/Grupo-38-Orange-Juice/orange-portfolio-back/domain/use-cases"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
-	db, err := postgres.Connect()
+	postgresDb, err := postgres.Connect()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	err = postgres.CreateDb(db)
+	err = postgres.CreateDb(postgresDb)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer db.Close()
+	defer postgresDb.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Olá, Squad 38!")
-	})
+	userRepo := postgres.NewUserRepository(postgresDb)
+	crypto := bcrypt.NewBcrypt()
+	userUseCase := usecases.NewUserUseCase(userRepo, crypto)
+	userController := http.NewUserController(userUseCase)
 
-	http.ListenAndServe(":8080", nil)
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server Petstore server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "petstore.swagger.io"
+	docs.SwaggerInfo.BasePath = "/v2"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+	r := http.Router()
+	r.POST("/users", userController.CreateUser)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	if err := r.Run(":8080"); err != nil {
+		fmt.Println(err)
+		return
+	}
 }

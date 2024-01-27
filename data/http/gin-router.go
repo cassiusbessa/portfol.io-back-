2cb05e51c79b71fb706b6b695b27dcffbf955336
cassius <cassiusbessa@gmail.com>
2024-01-26 22:45:58 -0300

    feat: :sparkles: add cors to gin router

--- data/http/gin-router.go
+++ data/http/gin-router.go
@@ -1,11 +1,21 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func Router() *gin.Engine {
 	ginMode := "release"
 	gin.SetMode(ginMode)
 	r := gin.New()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	r.Use(cors.New(config))
+
 	r.Use(gin.Recovery())
+
 	return r
 }

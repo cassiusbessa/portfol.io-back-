968be2502cd9d24520265d717751377a5c7f80ef
cassius <cassiusbessa@gmail.com>
2024-01-28 03:40:18 -0300

    test: :test_tube: implement update project test

---
+++ domain/use-cases/tests/update-project_test.go
@@ -0,0 +1,50 @@
+package usecases__test
+
+import (
+	"testing"
+
+	"github.com/Grupo-38-Orange-Juice/orange-portfolio-back/domain/entities"
+	usecases "github.com/Grupo-38-Orange-Juice/orange-portfolio-back/domain/use-cases"
+	data_mocks "github.com/Grupo-38-Orange-Juice/orange-portfolio-back/domain/use-cases/tests/mocks"
+)
+
+func TestUpdateProjectUseCase(t *testing.T) {
+	testcases := []struct {
+		name        string
+		projectRepo usecases.ProjectRepository
+		userRepo    usecases.UserRepository
+		wantErr     bool
+	}{
+		{
+			name:        "should return nil when project doesn't exist",
+			projectRepo: data_mocks.NewMockProjectRepository(false),
+			userRepo:    data_mocks.NewMockUserRepository(true),
+			wantErr:     false,
+		},
+		{
+			name:        "should return error when there is already a project with the same name from the same user",
+			projectRepo: data_mocks.NewMockProjectRepository(true),
+			userRepo:    data_mocks.NewMockUserRepository(true),
+			wantErr:     true,
+		},
+		{
+			name:        "should return error when user doesn't exist",
+			projectRepo: data_mocks.NewMockProjectRepository(false),
+			userRepo:    data_mocks.NewMockUserRepository(false),
+			wantErr:     true,
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			projectUseCase := usecases.NewProjectUseCase(tc.projectRepo, tc.userRepo)
+			project := &entities.Project{}
+			err := projectUseCase.UpdateProject(project, "any_id")
+			if tc.wantErr && err == nil {
+				t.Errorf("expected %v but got nil", tc.wantErr)
+			}
+			if !tc.wantErr && err != nil {
+				t.Errorf("expected nil but got %v", err)
+			}
+		})
+	}
+}

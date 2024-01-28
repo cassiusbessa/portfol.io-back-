56720640bcaa7511c7c22ebb52d2cbb7ebee5cf5
cassius <cassiusbessa@gmail.com>
2024-01-28 01:34:18 -0300

    feat: :sparkles: create project repository interface

--- domain/use-cases/repository.go
+++ domain/use-cases/repository.go
@@ -5,4 +5,14 @@ import "github.com/Grupo-38-Orange-Juice/orange-portfolio-back/domain/entities"
 type UserRepository interface {
 	CreateUser(user *entities.User) error
 	FindUserByEmail(email string) (*entities.User, error)
+	FindUserById(id string) (*entities.User, error)
+}
+
+type ProjectRepository interface {
+	CreateProject(project *entities.Project, userId string) error
+	FindAllProjects() ([]entities.Project, error)
+	FindProjectsByUserId(userId string) ([]entities.Project, error)
+	FindProjectByNameAndUserId(name, userId string) (*entities.Project, error)
+	UpdateProject(project *entities.Project) (*entities.Project, error)
+	DeleteProject(projectId string) error
 }
